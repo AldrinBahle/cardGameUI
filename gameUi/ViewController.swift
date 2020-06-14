15968df3fdd47ce0709ed810017a96088bdd2405
aldrinbgama <aldrin.gama@umuzi.org>
2020-06-14 02:12:23 +0200

    This is a new commit for what I originally planned to be amended

---
+++ gameUi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gameUi
+//
+//  Created by Aldrin Bahle Gama on 2020/06/13.
+//  Copyright © 2020 Aldrin Bahle Gama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
